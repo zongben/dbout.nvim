a119869e97ae4d85b0d84e4d6d6976631333a060
ben <ben76020567@gmail.com>
2025-09-19 18:43:38 +0800

    refac var name

--- lua/dbout/ui/init.lua
+++ lua/dbout/ui/init.lua
@@ -2,16 +2,16 @@ local db_explorer = require("dbout.ui.db_explorer")
 
 local inited = false
 
-local dbout_buf_name = "dbout://dbout.nvim"
-local db_explorer_buf_name = "dbout://db explorer"
+local main_buf_name = "dbout://dbout.nvim"
+local explorer_buf_name = "dbout://dbout explorer"
 
-local db_explorer_filetype = "db_explorer"
-local db_explorer_buf_var = {
+local explorer_filetype = "dbout_explorer"
+local explorer_buf_var = {
   is_explorer = "is_explorer",
 }
 
-local dbout_bufnr
-local db_explorer_bufnr
+local main_bufnr
+local explorer_bufnr
 
 local switch_win_to_buf = function(bufnr)
   local win = vim.api.nvim_get_current_win()
@@ -21,38 +21,38 @@ end
 local M = {}
 
 M.open_dbout = function()
-  if dbout_bufnr then
-    switch_win_to_buf(dbout_bufnr)
+  if main_bufnr then
+    switch_win_to_buf(main_bufnr)
     return
   end
 
-  dbout_bufnr = vim.api.nvim_create_buf(true, false)
-  vim.api.nvim_buf_set_name(dbout_bufnr, dbout_buf_name)
-  switch_win_to_buf(dbout_bufnr)
+  main_bufnr = vim.api.nvim_create_buf(true, false)
+  vim.api.nvim_buf_set_name(main_bufnr, main_buf_name)
+  switch_win_to_buf(main_bufnr)
 end
 
 M.open_db_explorer = function()
-  if not dbout_bufnr or dbout_bufnr ~= vim.api.nvim_get_current_buf() then
+  if not main_bufnr or main_bufnr ~= vim.api.nvim_get_current_buf() then
     return
   end
 
-  if db_explorer_bufnr == nil then
-    db_explorer_bufnr = vim.api.nvim_create_buf(false, true)
-    vim.api.nvim_buf_set_name(db_explorer_bufnr, db_explorer_buf_name)
-    vim.api.nvim_set_option_value("filetype", db_explorer_filetype, { buf = db_explorer_bufnr })
-    vim.api.nvim_buf_set_var(db_explorer_bufnr, db_explorer_buf_var.is_explorer, true)
+  if explorer_bufnr == nil then
+    explorer_bufnr = vim.api.nvim_create_buf(false, true)
+    vim.api.nvim_buf_set_name(explorer_bufnr, explorer_buf_name)
+    vim.api.nvim_set_option_value("filetype", explorer_filetype, { buf = explorer_bufnr })
+    vim.api.nvim_buf_set_var(explorer_bufnr, explorer_buf_var.is_explorer, true)
   end
 
   vim.cmd("vsplit")
   vim.cmd("vertical resize 30")
 
-  switch_win_to_buf(db_explorer_bufnr)
-  db_explorer.render(db_explorer_bufnr)
+  switch_win_to_buf(explorer_bufnr)
+  db_explorer.render(explorer_bufnr)
 end
 
 M.close_db_explorer = function()
-  if db_explorer_bufnr and vim.api.nvim_buf_is_loaded(db_explorer_bufnr) then
-    vim.api.nvim_buf_delete(db_explorer_bufnr, { unload = true })
+  if explorer_bufnr and vim.api.nvim_buf_is_loaded(explorer_bufnr) then
+    vim.api.nvim_buf_delete(explorer_bufnr, { unload = true })
   end
 end
 
@@ -66,20 +66,20 @@ M.init = function()
   vim.api.nvim_create_autocmd({ "BufDelete" }, {
     callback = function(args)
       local buf = args.buf
-      if vim.api.nvim_buf_get_name(buf) == dbout_buf_name then
-        dbout_bufnr = nil
+      if vim.api.nvim_buf_get_name(buf) == main_buf_name then
+        main_bufnr = nil
         return
       end
 
-      if vim.b[buf] and vim.b[buf][db_explorer_buf_var.is_explorer] then
-        db_explorer_bufnr = nil
+      if vim.b[buf] and vim.b[buf][explorer_buf_var.is_explorer] then
+        explorer_bufnr = nil
         return
       end
     end,
   })
 
   vim.api.nvim_create_autocmd("FileType", {
-    pattern = "db_explorer",
+    pattern = explorer_filetype,
     callback = function(args)
       local buf = args.buf
       db_explorer.set_keymaps(M, buf)
