ca2c1adf083e5674d12dab74daa1593313fc2095
ben <ben76020567@gmail.com>
2025-09-20 14:25:58 +0800

    fix ui behave

--- lua/dbout/ui/init.lua
+++ lua/dbout/ui/init.lua
@@ -14,8 +14,16 @@ local main_bufnr
 local explorer_bufnr
 
 local switch_win_to_buf = function(bufnr)
-  local win = vim.api.nvim_get_current_win()
-  vim.api.nvim_win_set_buf(win, bufnr)
+  local win = vim.fn.win_findbuf(bufnr)
+  local winnr
+  if #win > 0 then
+    winnr = win[1]
+  else
+    winnr = vim.api.nvim_get_current_win()
+  end
+
+  vim.api.nvim_win_set_buf(winnr, bufnr)
+  vim.api.nvim_set_current_win(winnr)
 end
 
 local M = {}
@@ -42,18 +50,24 @@ M.open_db_explorer = function()
     vim.api.nvim_buf_set_name(explorer_bufnr, explorer_buf_name)
     vim.api.nvim_set_option_value("filetype", explorer_filetype, { buf = explorer_bufnr })
     vim.api.nvim_buf_set_var(explorer_bufnr, explorer_buf_var.is_explorer, true)
+    db_explorer.set_keymaps(M, explorer_bufnr)
   end
 
-  vim.cmd("vsplit")
-  vim.cmd("vertical resize 30")
+  if vim.fn.bufwinnr(explorer_bufnr) == -1 then
+    vim.cmd("vsplit")
+    vim.cmd("vertical resize 30")
+    db_explorer.render(explorer_bufnr)
+  end
 
   switch_win_to_buf(explorer_bufnr)
-  db_explorer.render(explorer_bufnr)
 end
 
 M.close_db_explorer = function()
   if explorer_bufnr and vim.api.nvim_buf_is_loaded(explorer_bufnr) then
-    vim.api.nvim_buf_delete(explorer_bufnr, { unload = true })
+    local wins = vim.fn.win_findbuf(explorer_bufnr)
+    if #wins > 0 then
+      vim.api.nvim_win_close(wins[1], true)
+    end
   end
 end
 
@@ -71,19 +85,6 @@ M.init = function()
         main_bufnr = nil
         return
       end
-
-      if vim.b[buf] and vim.b[buf][explorer_buf_var.is_explorer] then
-        explorer_bufnr = nil
-        return
-      end
-    end,
-  })
-
-  vim.api.nvim_create_autocmd("FileType", {
-    pattern = explorer_filetype,
-    callback = function(args)
-      local buf = args.buf
-      db_explorer.set_keymaps(M, buf)
     end,
   })
 
